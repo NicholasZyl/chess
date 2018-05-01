af33eb6503c3ce3fc67390e7cc04607584718f92
NicholasZyl <mzylkowski@gmail.com>
2018-05-01 11:42:36 +0200

    Adding specification for instantiating Piece object through factory method

--- spec/Domain/PieceSpec.php
+++ spec/Domain/PieceSpec.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 namespace spec\NicholasZyl\Chess\Domain;
 
 use NicholasZyl\Chess\Domain\Piece;
+use NicholasZyl\Chess\Domain\Piece\Rank;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class PieceSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_generates_piece_from_rank_and_color()
     {
+        $this->beConstructedThrough(
+            'fromRankAndColor',
+            [
+                Rank::fromString('king'),
+                Color::fromString('white')
+            ]
+        );
         $this->shouldHaveType(Piece::class);
     }
 }
