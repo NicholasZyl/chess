0f1c21948f3dd45a49fa0dd58302f26900e1f786
NicholasZyl <mzylkowski@gmail.com>
2018-05-01 21:56:33 +0200

    Implementing bishop's movement rules

--- spec/Domain/Rules/Fide/BishopMovementRulesSpec.php
+++ spec/Domain/Rules/Fide/BishopMovementRulesSpec.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace spec\NicholasZyl\Chess\Domain\Rules\Fide;
 
+use NicholasZyl\Chess\Domain\Chessboard\Coordinates;
+use NicholasZyl\Chess\Domain\Chessboard\Exception\IllegalMove;
 use NicholasZyl\Chess\Domain\Piece\Rank;
 use NicholasZyl\Chess\Domain\Rules\Fide\BishopMovementRules;
 use NicholasZyl\Chess\Domain\Rules\MovementRules;
@@ -23,5 +25,61 @@ class BishopMovementRulesSpec extends ObjectBehavior
     function it_is_for_bishop()
     {
         $this->isFor()->shouldBeLike(Rank::bishop());
+    }
+
+    function it_allows_moving_diagonally_forward_kingside()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('f6');
+
+        $this->validate($from, $to);
+    }
+
+    function it_allows_moving_diagonally_forward_queenside()
+    {
+        $from = Coordinates::fromString('d4');
+        $to = Coordinates::fromString('a7');
+
+        $this->validate($from, $to);
+    }
+
+    function it_allows_moving_diagonally_backward_kingside()
+    {
+        $from = Coordinates::fromString('d4');
+        $to = Coordinates::fromString('e3');
+
+        $this->validate($from, $to);
+    }
+
+    function it_allows_moving_diagonally_backward_queenside()
+    {
+        $from = Coordinates::fromString('d4');
+        $to = Coordinates::fromString('b2');
+
+        $this->validate($from, $to);
+    }
+
+    function it_disallows_moving_vertically()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('f3');
+
+        $this->shouldThrow(new IllegalMove($from, $to))->during('validate', [$from, $to,]);
+    }
+
+    function it_disallows_moving_horizontally()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('c4');
+
+        $this->shouldThrow(new IllegalMove($from, $to))->during('validate', [$from, $to,]);
+    }
+
+    function it_disallows_moving_in_other_directions()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('e4');
+
+        $this->shouldThrow(new IllegalMove($from, $to))->during('validate', [$from, $to,]);
     }
 }
