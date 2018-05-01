bae86e97647535d549e06727f10cb3e6fdefbec6
NicholasZyl <mzylkowski@gmail.com>
2018-05-01 12:01:38 +0200

    Implementing first scenario step with simplest possible implementation

---
+++ spec/Domain/ChessboardSpec.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace spec\NicholasZyl\Chess\Domain;
+
+use NicholasZyl\Chess\Domain\Board\Coordinates;
+use NicholasZyl\Chess\Domain\Chessboard;
+use NicholasZyl\Chess\Domain\Color;
+use NicholasZyl\Chess\Domain\Piece;
+use PhpSpec\ObjectBehavior;
+
+class ChessboardSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Chessboard::class);
+    }
+
+    public function it_allows_placing_piece_at_given_coordinates()
+    {
+        $piece = Piece::fromRankAndColor(Piece\Rank::fromString('king'), Color::fromString('white'));
+        $coordinates = Coordinates::fromString('B2');
+
+        $this->placePieceAtCoordinates($piece, $coordinates);
+    }
+}
---
+++ src/Domain/Chessboard.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+
+
+namespace NicholasZyl\Chess\Domain;
+
+final class Chessboard
+{
+    public function placePieceAtCoordinates($argument1, $argument2)
+    {
+        // TODO: write logic here
+    }
+}
