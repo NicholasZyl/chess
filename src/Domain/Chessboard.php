017cc3c0d477c29b5863479b120f286a91c682d0
NicholasZyl <mzylkowski@gmail.com>
2018-05-01 12:23:51 +0200

    Starting with actual implementation

--- src/Domain/Chessboard.php
+++ src/Domain/Chessboard.php
@@ -4,21 +4,34 @@ declare(strict_types=1);
 namespace NicholasZyl\Chess\Domain;
 
 use NicholasZyl\Chess\Domain\Board\Coordinates;
+use NicholasZyl\Chess\Domain\Board\Square;
 
 final class Chessboard
 {
-    public function placePieceAtCoordinates(Piece $piece, Coordinates $coordinates)
+    /**
+     * @var Square[]
+     */
+    private $squares = [];
+
+    public function placePieceAtCoordinates(Piece $piece, Coordinates $coordinates): void
     {
         // TODO: write logic here
     }
 
-    public function movePiece(Coordinates $source, Coordinates $destination)
+    public function movePiece(Coordinates $source, Coordinates $destination): void
     {
         // TODO: write logic here
     }
 
-    public function hasPieceAtCoordinates($argument1, $argument2)
+    public function hasPieceAtCoordinates(Piece $piece, Coordinates $coordinates): bool
     {
-        // TODO: write logic here
+        $square = $this->getSquareAt($coordinates);
+
+        return $piece->isSameAs($square->pickPiece());
+    }
+
+    private function getSquareAt(Coordinates $coordinates): Square
+    {
+        return $this->squares[(string) $coordinates];
     }
 }
