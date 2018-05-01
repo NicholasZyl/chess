2b06734d2c9a53fd384d04ab584c2a0a82001119
NicholasZyl <mzylkowski@gmail.com>
2018-05-01 13:02:54 +0200

    Adding picking piece from a square with naive implementation

--- spec/Domain/Board/SquareSpec.php
+++ spec/Domain/Board/SquareSpec.php
@@ -5,14 +5,24 @@ namespace spec\NicholasZyl\Chess\Domain\Board;
 
 use NicholasZyl\Chess\Domain\Board\Coordinates;
 use NicholasZyl\Chess\Domain\Board\Square;
+use NicholasZyl\Chess\Domain\Piece;
 use PhpSpec\ObjectBehavior;
 
 class SquareSpec extends ObjectBehavior
 {
-    public function it_is_created_for_chessboard_coordinates()
+    function let()
     {
         $coordinates = Coordinates::fromString('A1');
         $this->beConstructedThrough('forCoordinates', [$coordinates]);
+    }
+
+    public function it_is_created_for_chessboard_coordinates()
+    {
         $this->shouldHaveType(Square::class);
+    }
+
+    public function it_allows_to_pick_piece_placed_on_it()
+    {
+        $this->pickPiece()->shouldBeAnInstanceOf(Piece::class);
     }
 }
--- src/Domain/Board/Square.php
+++ src/Domain/Board/Square.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace NicholasZyl\Chess\Domain\Board;
 
+use NicholasZyl\Chess\Domain\Color;
+use NicholasZyl\Chess\Domain\Piece;
+
 final class Square
 {
     private function __construct()
@@ -16,5 +19,10 @@ final class Square
         // TODO: write logic here
 
         return $square;
+    }
+
+    public function pickPiece(): Piece
+    {
+        return Piece::fromRankAndColor(Piece\Rank::fromString('queen'), Color::black());
     }
 }
