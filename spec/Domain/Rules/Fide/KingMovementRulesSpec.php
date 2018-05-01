efd79ec3f51370ee4cb1fc70dee280b66c7e8dd2
NicholasZyl <mzylkowski@gmail.com>
2018-05-01 21:14:50 +0200

    Adding more examples for king's moves

--- spec/Domain/Rules/Fide/KingMovementRulesSpec.php
+++ spec/Domain/Rules/Fide/KingMovementRulesSpec.php
@@ -43,10 +43,74 @@ class KingMovementRulesSpec extends ObjectBehavior
         $this->shouldThrow(new IllegalMove($from, $to))->during('validate', [$from, $to,]);
     }
 
+    function it_allows_moving_one_square_backward()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('c2');
+
+        $this->validate($from, $to);
+    }
+
+    function it_allows_moving_one_square_to_the_queenside()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('b3');
+
+        $this->validate($from, $to);
+    }
+
+    function it_allows_moving_one_square_to_the_kingside()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('d3');
+
+        $this->validate($from, $to);
+    }
+
     function it_disallows_moving_more_than_one_file()
     {
         $from = Coordinates::fromString('c3');
         $to = Coordinates::fromString('e3');
+
+        $this->shouldThrow(new IllegalMove($from, $to))->during('validate', [$from, $to,]);
+    }
+
+    function it_allows_moving_one_square_diagonally_forward_queenside()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('b4');
+
+        $this->validate($from, $to);
+    }
+
+    function it_allows_moving_one_square_diagonally_forward_kingside()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('d4');
+
+        $this->validate($from, $to);
+    }
+
+    function it_allows_moving_one_square_diagonally_backward_queenside()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('b2');
+
+        $this->validate($from, $to);
+    }
+
+    function it_allows_moving_one_square_diagonally_backward_kingside()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('d2');
+
+        $this->validate($from, $to);
+    }
+
+    function it_disallows_moving_more_than_one_square_diagonally()
+    {
+        $from = Coordinates::fromString('c3');
+        $to = Coordinates::fromString('a1');
 
         $this->shouldThrow(new IllegalMove($from, $to))->during('validate', [$from, $to,]);
     }
