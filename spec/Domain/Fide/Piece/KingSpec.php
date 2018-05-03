c4b315a90e042ba0c1fabbad2a134a875ff5e140
NicholasZyl <mzylkowski@gmail.com>
2018-05-03 22:41:09 +0200

    Refactoring - adjusting for new movement classes - king

--- spec/Domain/Fide/Piece/KingSpec.php
+++ spec/Domain/Fide/Piece/KingSpec.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace spec\NicholasZyl\Chess\Domain\Fide\Piece;
 
 use NicholasZyl\Chess\Domain\Chessboard\Exception\IllegalMove;
+use NicholasZyl\Chess\Domain\Chessboard\Move\AlongDiagonal;
+use NicholasZyl\Chess\Domain\Chessboard\Move\AlongFile;
+use NicholasZyl\Chess\Domain\Chessboard\Move\AlongRank;
+use NicholasZyl\Chess\Domain\Chessboard\Move\NearestNotSameFileRankOrDiagonal;
 use NicholasZyl\Chess\Domain\Chessboard\Square\CoordinatePair;
 use NicholasZyl\Chess\Domain\Fide\Piece\King;
 use NicholasZyl\Chess\Domain\Piece;
@@ -31,6 +35,64 @@ class KingSpec extends ObjectBehavior
         $pawn = King::forColor(Piece\Color::white());
 
         $this->isSameAs($pawn)->shouldBe(true);
+    }
+
+    function it_can_move_along_file()
+    {
+        $from = CoordinatePair::fromFileAndRank('a', 1);
+        $to = CoordinatePair::fromFileAndRank('a', 2);
+        $move = AlongFile::between(
+            $from,
+            $to
+        );
+
+        $this->mayMove($move);
+    }
+
+    function it_can_move_along_rank()
+    {
+        $from = CoordinatePair::fromFileAndRank('a', 1);
+        $to = CoordinatePair::fromFileAndRank('b', 1);
+        $move = AlongRank::between(
+            $from,
+            $to
+        );
+
+        $this->mayMove($move);
+    }
+
+    function it_can_move_along_diagonal()
+    {
+        $move = AlongDiagonal::between(
+            CoordinatePair::fromFileAndRank('a', 1),
+            CoordinatePair::fromFileAndRank('b', 2)
+        );
+
+        $this->mayMove($move);
+    }
+
+    function it_cannot_move_to_nearest_square()
+    {
+        $from = CoordinatePair::fromFileAndRank('a', 1);
+        $to = CoordinatePair::fromFileAndRank('c', 2);
+        $move = NearestNotSameFileRankOrDiagonal::between(
+            $from,
+            $to
+        );
+
+        $this->shouldThrow(IllegalMove::forMove($move))->during('mayMove', [$move,]);
+    }
+
+    function it_cannot_move_more_than_one_square()
+    {
+        $from = CoordinatePair::fromFileAndRank('a', 1);
+        $to = CoordinatePair::fromFileAndRank('a', 3);
+        $move = AlongFile::between(
+            $from,
+            $to
+        );
+
+        $this->shouldThrow(IllegalMove::forMove($move))->during('mayMove', [$move,]);
     }
 
     function it_can_move_to_adjoining_square_forward()
@@ -73,7 +135,7 @@ class KingSpec extends ObjectBehavior
         $this->intentMove($from, $to);
     }
 
-    function it_cannot_move_more_than_one_square()
+    function it_can_not_move_more_than_one_square()
     {
         $from = CoordinatePair::fromString('c3');
         $to = CoordinatePair::fromString('c5');
--- src/Domain/Fide/Piece/King.php
+++ src/Domain/Fide/Piece/King.php
@@ -3,22 +3,35 @@ declare(strict_types=1);
 
 namespace NicholasZyl\Chess\Domain\Fide\Piece;
 
+use NicholasZyl\Chess\Domain\Chessboard\ChessboardMove;
 use NicholasZyl\Chess\Domain\Chessboard\Exception\IllegalMove;
 use NicholasZyl\Chess\Domain\Chessboard\Move;
 use NicholasZyl\Chess\Domain\Chessboard\Square\CoordinatePair;
 
 final class King extends Piece
 {
+    private const ALLOWED_MOVE_DISTANCE = 1;
+
     /**
      * {@inheritdoc}
      */
     public function intentMove(CoordinatePair $from, CoordinatePair $to): Move
     {
         $move = Move::between($from, $to);
-        if ((!$move->isAlongDiagonal() && !$move->isAlongRank() && !$move->isAlongFile()) || $move->isAwayMoreSquaresThan(1)) {
+        if ((!$move->isAlongDiagonal() && !$move->isAlongRank() && !$move->isAlongFile()) || $move->isAwayMoreSquaresThan(self::ALLOWED_MOVE_DISTANCE)) {
             throw new IllegalMove($from, $to);
         }
 
         return $move;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function mayMove(ChessboardMove $move): void
+    {
+        if ($move instanceof Move\NearestNotSameFileRankOrDiagonal || count($move) > self::ALLOWED_MOVE_DISTANCE) {
+            throw IllegalMove::forMove($move);
+        }
     }
 }
