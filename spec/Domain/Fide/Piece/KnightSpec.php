74876d2e132992de7efbec4652c1492e01e80568
NicholasZyl <mzylkowski@gmail.com>
2018-05-10 22:32:33 +0200

    Adding move intending method for knight

--- spec/Domain/Fide/Piece/KnightSpec.php
+++ spec/Domain/Fide/Piece/KnightSpec.php
@@ -5,18 +5,17 @@ namespace spec\NicholasZyl\Chess\Domain\Fide\Piece;
 
 use NicholasZyl\Chess\Domain\Board;
 use NicholasZyl\Chess\Domain\Exception\Move\NotAllowedForPiece;
+use NicholasZyl\Chess\Domain\Exception\Move\ToIllegalPosition;
 use NicholasZyl\Chess\Domain\Exception\MoveNotAllowedForPiece;
 use NicholasZyl\Chess\Domain\Fide\Board\CoordinatePair;
 use NicholasZyl\Chess\Domain\Fide\Board\Direction\LShaped;
 use NicholasZyl\Chess\Domain\Fide\Move\AlongDiagonal;
 use NicholasZyl\Chess\Domain\Fide\Move\AlongFile;
 use NicholasZyl\Chess\Domain\Fide\Move\AlongRank;
-use NicholasZyl\Chess\Domain\Fide\Move\Capturing;
 use NicholasZyl\Chess\Domain\Fide\Move\NearestNotSameFileRankOrDiagonal;
 use NicholasZyl\Chess\Domain\Fide\Move\NotIntervened;
 use NicholasZyl\Chess\Domain\Fide\Move\OverOtherPieces;
 use NicholasZyl\Chess\Domain\Fide\Move\ToAdjoiningSquare;
-use NicholasZyl\Chess\Domain\Fide\Move\ToUnoccupiedSquare;
 use NicholasZyl\Chess\Domain\Fide\Piece\Knight;
 use NicholasZyl\Chess\Domain\Piece;
 use PhpSpec\ObjectBehavior;
@@ -47,12 +46,10 @@ class KnightSpec extends ObjectBehavior
 
     function it_may_move_to_one_of_the_squares_nearest_to_that_on_which_it_stands_but_not_on_same_rank_file_or_diagonal()
     {
-        $move = new ToUnoccupiedSquare(
-            new OverOtherPieces(
-                CoordinatePair::fromFileAndRank('a', 1),
-                CoordinatePair::fromFileAndRank('b', 3),
-                new LShaped()
-            )
+        $move = new OverOtherPieces(
+            CoordinatePair::fromFileAndRank('a', 1),
+            CoordinatePair::fromFileAndRank('b', 3),
+            new LShaped()
         );
 
         $this->canMove($move);
@@ -60,12 +57,10 @@ class KnightSpec extends ObjectBehavior
 
     function it_may_not_move_to_adjoining_square()
     {
-        $move = new ToUnoccupiedSquare(
-            new ToAdjoiningSquare(
-                CoordinatePair::fromFileAndRank('a', 1),
-                CoordinatePair::fromFileAndRank('b', 2),
-                new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongDiagonal()
-            )
+        $move = new ToAdjoiningSquare(
+            CoordinatePair::fromFileAndRank('a', 1),
+            CoordinatePair::fromFileAndRank('b', 2),
+            new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongDiagonal()
         );
 
         $this->shouldThrow(new NotAllowedForPiece($this->getWrappedObject(), $move))->during('canMove', [$move,]);
@@ -73,12 +68,10 @@ class KnightSpec extends ObjectBehavior
 
     function it_may_not_move_along_file()
     {
-        $move = new ToUnoccupiedSquare(
-            new OverOtherPieces(
-                CoordinatePair::fromFileAndRank('a', 1),
-                CoordinatePair::fromFileAndRank('a', 5),
-                new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongFile()
-            )
+        $move = new OverOtherPieces(
+            CoordinatePair::fromFileAndRank('a', 1),
+            CoordinatePair::fromFileAndRank('a', 5),
+            new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongFile()
         );
 
         $this->shouldThrow(new NotAllowedForPiece($this->getWrappedObject(), $move))->during('canMove', [$move,]);
@@ -86,12 +79,10 @@ class KnightSpec extends ObjectBehavior
 
     function it_may_not_move_along_rank()
     {
-        $move = new ToUnoccupiedSquare(
-            new NotIntervened(
-                CoordinatePair::fromFileAndRank('a', 5),
-                CoordinatePair::fromFileAndRank('f', 5),
-                new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongRank()
-            )
+        $move = new OverOtherPieces(
+            CoordinatePair::fromFileAndRank('a', 5),
+            CoordinatePair::fromFileAndRank('f', 5),
+            new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongRank()
         );
 
         $this->shouldThrow(new NotAllowedForPiece($this->getWrappedObject(), $move))->during('canMove', [$move,]);
@@ -99,12 +90,21 @@ class KnightSpec extends ObjectBehavior
 
     function it_may_not_move_along_diagonal()
     {
-        $move = new ToUnoccupiedSquare(
-            new NotIntervened(
-                CoordinatePair::fromFileAndRank('h', 8),
-                CoordinatePair::fromFileAndRank('a', 1),
-                new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongDiagonal()
-            )
+        $move = new OverOtherPieces(
+            CoordinatePair::fromFileAndRank('h', 8),
+            CoordinatePair::fromFileAndRank('a', 1),
+            new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongDiagonal()
+        );
+
+        $this->shouldThrow(new NotAllowedForPiece($this->getWrappedObject(), $move))->during('canMove', [$move,]);
+    }
+
+    function it_may_not_be_blocked_by_intervening_pieces()
+    {
+        $move = new NotIntervened(
+            CoordinatePair::fromFileAndRank('h', 8),
+            CoordinatePair::fromFileAndRank('a', 1),
+            new \NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongDiagonal()
         );
 
         $this->shouldThrow(new NotAllowedForPiece($this->getWrappedObject(), $move))->during('canMove', [$move,]);
@@ -112,15 +112,38 @@ class KnightSpec extends ObjectBehavior
 
     function it_may_capture_the_same_way_it_moves()
     {
-        $move = new Capturing(
-            new OverOtherPieces(
-                CoordinatePair::fromFileAndRank('a', 1),
-                CoordinatePair::fromFileAndRank('b', 3),
-                new LShaped()
-            )
+        $move = new OverOtherPieces(
+            CoordinatePair::fromFileAndRank('a', 1),
+            CoordinatePair::fromFileAndRank('b', 3),
+            new LShaped()
         );
 
         $this->canMove($move);
+    }
+
+    function it_intents_move_to_adjoining_square()
+    {
+        $source = CoordinatePair::fromFileAndRank('d', 3);
+        $destination = CoordinatePair::fromFileAndRank('e', 1);
+
+        $this->placeAt($source);
+        $this->intentMoveTo($destination)->shouldBeLike(
+            new OverOtherPieces(
+                $source,
+                $destination,
+                new LShaped()
+            )
+        );
+    }
+
+    function it_may_not_intent_move_to_illegal_position()
+    {
+        $source = CoordinatePair::fromFileAndRank('a', 1);
+        $destination = CoordinatePair::fromFileAndRank('a', 3);
+
+        $this->placeAt($source);
+
+        $this->shouldThrow(new ToIllegalPosition($this->getWrappedObject(), $source, $destination))->during('intentMoveTo', [$destination,]);
     }
 
 
--- src/Domain/Fide/Piece/Knight.php
+++ src/Domain/Fide/Piece/Knight.php
@@ -5,7 +5,9 @@ namespace NicholasZyl\Chess\Domain\Fide\Piece;
 
 use NicholasZyl\Chess\Domain\Board;
 use NicholasZyl\Chess\Domain\BoardMove;
+use NicholasZyl\Chess\Domain\Exception\InvalidDirection;
 use NicholasZyl\Chess\Domain\Exception\Move\NotAllowedForPiece;
+use NicholasZyl\Chess\Domain\Exception\Move\ToIllegalPosition;
 use NicholasZyl\Chess\Domain\Exception\MoveNotAllowedForPiece;
 use NicholasZyl\Chess\Domain\Fide\Board\Direction\LShaped;
 use NicholasZyl\Chess\Domain\Fide\Move\NearestNotSameFileRankOrDiagonal;
@@ -14,6 +16,11 @@ use NicholasZyl\Chess\Domain\Move;
 
 final class Knight extends Piece
 {
+    /**
+     * @var Board\Coordinates
+     */
+    private $position;
+
     /**
      * {@inheritdoc}
      */
@@ -39,6 +46,40 @@ final class Knight extends Piece
     {
         if (!$move->is(OverOtherPieces::class) || !$move->inDirection(new LShaped())) {
             throw new NotAllowedForPiece($this, $move);
+        }
+    }
+
+    /**
+     * Place piece at given coordinates.
+     *
+     * @param Board\Coordinates $coordinates
+     *
+     * @return void
+     */
+    public function placeAt(Board\Coordinates $coordinates): void
+    {
+        $this->position = $coordinates;
+    }
+
+    /**
+     * Intent move from piece's current position to the destination.
+     *
+     * @param Board\Coordinates $destination
+     *
+     * @throws ToIllegalPosition
+     *
+     * @return BoardMove
+     */
+    public function intentMoveTo(Board\Coordinates $destination): BoardMove
+    {
+        try {
+            return new OverOtherPieces(
+                $this->position,
+                $destination,
+                new LShaped()
+            );
+        } catch (InvalidDirection $exception) {
+            throw new ToIllegalPosition($this, $this->position, $destination);
         }
     }
 }
