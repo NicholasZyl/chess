fe4e9741e487f5820c9e1c5e5c4a0da134915150
NicholasZyl <mzylkowski@gmail.com>
2018-05-29 00:34:21 +0200

    Adding queen moves rules class

---
+++ spec/Domain/Fide/Rules/QueenMovesSpec.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+
+namespace spec\NicholasZyl\Chess\Domain\Fide\Rules;
+
+use NicholasZyl\Chess\Domain\Exception\IllegalMove\MoveNotAllowedForPiece;
+use NicholasZyl\Chess\Domain\Fide\Board\CoordinatePair;
+use NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongDiagonal;
+use NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongFile;
+use NicholasZyl\Chess\Domain\Fide\Board\Direction\AlongRank;
+use NicholasZyl\Chess\Domain\Fide\Move\NotIntervened;
+use NicholasZyl\Chess\Domain\Fide\Move\OverOtherPieces;
+use NicholasZyl\Chess\Domain\Fide\Piece\Knight;
+use NicholasZyl\Chess\Domain\Fide\Piece\Queen;
+use NicholasZyl\Chess\Domain\Fide\Rules\QueenMoves;
+use NicholasZyl\Chess\Domain\Piece\Color;
+use NicholasZyl\Chess\Domain\Rules\PieceMoves;
+use PhpSpec\ObjectBehavior;
+
+class QueenMovesSpec extends ObjectBehavior
+{
+    /**
+     * @var Queen
+     */
+    private $queen;
+
+    function let()
+    {
+        $this->queen = Queen::forColor(Color::white());
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(QueenMoves::class);
+    }
+
+    function it_is_piece_moves_rule()
+    {
+        $this->shouldBeAnInstanceOf(PieceMoves::class);
+    }
+
+    function it_is_applicable_for_queen()
+    {
+        $this->areApplicableFor($this->queen)->shouldBe(true);
+    }
+
+    function it_is_not_applicable_for_other_pieces()
+    {
+        $this->areApplicableFor(Knight::forColor(Color::white()))->shouldBe(false);
+    }
+
+    function it_verifies_as_valid_move_to_any_square_along_file()
+    {
+        $move = new NotIntervened(
+            CoordinatePair::fromFileAndRank('a', 1),
+            CoordinatePair::fromFileAndRank('a', 2),
+            new AlongFile()
+        );
+
+        $this->verify($this->queen, $move);
+    }
+
+    function it_verifies_as_valid_move_to_any_square_along_rank()
+    {
+        $move = new NotIntervened(
+            CoordinatePair::fromFileAndRank('g', 1),
+            CoordinatePair::fromFileAndRank('b', 1),
+            new AlongRank()
+        );
+
+        $this->verify($this->queen, $move);
+    }
+
+    function it_verifies_as_valid_move_to_any_square_along_diagonal()
+    {
+        $move = new NotIntervened(
+            CoordinatePair::fromFileAndRank('d', 3),
+            CoordinatePair::fromFileAndRank('b', 1),
+            new AlongDiagonal()
+        );
+
+        $this->verify($this->queen, $move);
+    }
+
+    function it_verifies_as_invalid_move_over_any_intervening_pieces()
+    {
+        $move = new OverOtherPieces(
+            CoordinatePair::fromFileAndRank('a', 1),
+            CoordinatePair::fromFileAndRank('b', 2),
+            new AlongDiagonal()
+        );
+
+        $this->shouldThrow(new MoveNotAllowedForPiece($this->queen, $move))->during('verify', [$this->queen, $move,]);
+    }
+}
---
+++ src/Domain/Fide/Rules/QueenMoves.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace NicholasZyl\Chess\Domain\Fide\Rules;
+
+use NicholasZyl\Chess\Domain\Event;
+use NicholasZyl\Chess\Domain\Exception\IllegalMove\MoveNotAllowedForPiece;
+use NicholasZyl\Chess\Domain\Fide\Board\Direction\LShaped;
+use NicholasZyl\Chess\Domain\Fide\Move\NotIntervened;
+use NicholasZyl\Chess\Domain\Fide\Piece\Piece;
+use NicholasZyl\Chess\Domain\Fide\Piece\Queen;
+use NicholasZyl\Chess\Domain\Move;
+use NicholasZyl\Chess\Domain\Rules\PieceMoves;
+
+final class QueenMoves implements PieceMoves
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function areApplicableFor(Piece $piece): bool
+    {
+        return $piece instanceof Queen;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function verify(Piece $piece, Move $move): void
+    {
+        if (!$move instanceof NotIntervened || $move->inDirection(new LShaped())) {
+            throw new MoveNotAllowedForPiece($piece, $move);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function applyAfter(Event $event): void
+    {
+        // No specific rules to apply.
+    }
+}
